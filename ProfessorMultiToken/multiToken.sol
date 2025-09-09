c22540d55562301dd14e25e27a7891c536a84bc4
Pranavkok <pranavkokoff@gmail.com>
2025-09-09 19:54:22 +0530

    Update multiToken.sol

--- ProfessorMultiToken/multiToken.sol
+++ ProfessorMultiToken/multiToken.sol
@@ -6,6 +6,7 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {ERC1155Pausable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Professor is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
     uint256 public publicPrice = 0.001 ether ;
@@ -36,6 +37,16 @@ contract Professor is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
         require(id < 10 , "This NFT does not exist !");
         require(totalSupply(id) + amount <= maxSupply , "Sorry we are out of stock :( ");
         _mint(msg.sender, id, amount, "");
+    }
+
+    function withdraw(address _address) external onlyOwner{
+        uint256 balance = address(this).balance ;
+        payable(_address).transfer(balance) ;
+    }
+
+    function uri(uint256 _id) public view virtual override returns (string memory) {
+        require(exists(_id),"Token with this Id do not exists !");
+        return string(abi.encodePacked(super.uri(_id), Strings.toString(_id), ".json"));
     }
 
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
