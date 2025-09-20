9a6e40a1383c17181e853858ad62256d7d2d24d1
Pranavkok <pranavkokoff@gmail.com>
2025-09-20 14:45:57 +0530

    Update Coin.sol

--- ProfessorCoin/contracts/Coin.sol
+++ ProfessorCoin/contracts/Coin.sol
@@ -1,1 +1,16 @@
 
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.4.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract ProfessorCoin is ERC20, ERC20Permit {
+    constructor(address recipient)
+        ERC20("ProfessorCoin", "PFC")
+        ERC20Permit("ProfessorCoin")
+    {
+        _mint(recipient, 100 * 10 ** decimals());
+    }
+}
